07c7b2486e49f2d7b5fcbc9231943cf73717ee68
Al <battatotal@yandex.ru>
2023-08-04 00:48:29 +0300

    поменял url для API Post

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -7,8 +7,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blogapp.urls', namespace='blog')),
     path('api/list/', UsersApiList.as_view()),
-    #для get запроса параметром идет id пользователя, для других запросов - id поста
+    #ниже адрес для двух типов запросов(для get запроса параметром идет id пользователя, для delete - id поста)
     path('api/author_post_list/<int:pk>/', PostsApi.as_view()),
+
+    #post
+    path('api/author_post_list/', PostsApi.as_view()),
 
 
 
